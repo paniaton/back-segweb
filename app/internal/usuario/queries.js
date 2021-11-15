5f5415f04d4e82c2be744cd8d6d34e671f102fa2
paniaton <lucpanizz@gmail.com>
2021-11-14 21:36:39 -0300

    change search user by mail

--- app/internal/usuario/queries.js
+++ app/internal/usuario/queries.js
@@ -1,6 +1,6 @@
 
 const findAllUsers = `SELECT id, nombre, mail nombre FROM saw_db.usuario`;
-const findOneUser = `SELECT estado, motivo, fecha_modificacion FROM saw_db.usuario where id = ?`;
+const findOneUser = `SELECT estado, motivo, fecha_modificacion FROM saw_db.usuario where mail = ?`;
 const findUserSesion = `SELECT admin FROM saw_db.sesion where id = ? and token = ?`;
 
 exports.findAllUsers = findAllUsers;
